30988398ceaa4763ab1bd22c041e0c0eb7715d31
Sophia Golden <sophia.golden28@houghton.edu>
2025-04-30 17:12:55 -0400

    Basic To-Do code

---
+++ src/main/java/com/mycompany/todoapp/Mongo.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.todoapp;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+/**
+ *
+ * @author Sound Room
+ */
+public class Mongo {
+
+    public static final String DB_URL = "mongodb+srv://sophiagolden28:goldensophia@cluster0.ytb00zk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+    public static MongoClient getConnection() {
+
+        MongoClient mongoClient = MongoClients.create(
+                MongoClientSettings.builder()
+                        .applyConnectionString(new ConnectionString(DB_URL))
+                        .build());
+        return mongoClient;
+    }
+
+}
